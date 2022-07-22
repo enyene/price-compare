d3ae96f70b4ac6d72a9b2dae951e0fdd533b2f46
Enyene Akpan <akpanenyene@gmail.com>
2022-07-22 13:35:28 +0100

    work on app -accounts create views for login,register,logout and profile add authentication for the views

--- zuri/accounts/views.py
+++ zuri/accounts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .decorators import unauthenticated_user
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+# landing page
+def home(request):
+    return render(request, 'accounts/home.html')
+
+#login page
+@unauthenticated_user
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+# register page
+@unauthenticated_user
+def register(request):
+    return render(request, 'accounts/register.html')
+
+# logout page
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+ # profile page
+@login_required(login_url='/')
+def profile(request):
+    return render(request, 'accounts/profile.html')
